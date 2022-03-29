16caff6a935963f63dbf3aab392e4dcdda384851
fuliheng <funono99@gmail.com>
2022-03-29 15:00:32 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648537011,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/打地鼠_背景-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/圖片2-sheet0.png",
+		"images/圖片2-sheet1.png",
+		"media/boom.m4a",
+		"media/boom.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
